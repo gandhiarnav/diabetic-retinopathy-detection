f817dfbd579dac3b681b38d83a291924f2be835f
Arnav Gandhi <gandhiarnav10000@gmail.com>
2026-04-03 00:39:53 +0530

    Modified file paths in multimodal_data_preprocesing.py for kaggle

--- src/multimodal_data_preprocessing.py
+++ src/multimodal_data_preprocessing.py
@@ -51,21 +51,21 @@ if __name__ == "__main__":
     # If running locally, change these to your local paths (e.g., 'data/raw/idrid/...')
     
     # KAGGLE PATHS:
-    RAW_DIR = '/kaggle/input/indian-diabetic-retinopathy-image-dataset-idrid2/'
+    RAW_DIR = '/kaggle/input/datasets/mohamedabdalkader/indian-diabetic-retinopathy-image-dataset-idrid2/IDRiD'
     PROC_DIR = '/kaggle/working/data/processed/idrid/'
     
     # Train Data
     process_dataset(
-        raw_csv=os.path.join(RAW_DIR, 'train_labels.csv'),
-        raw_img_dir=os.path.join(RAW_DIR, 'train_images/'),
+        raw_csv=os.path.join(RAW_DIR, 'train/annotations.csv'),
+        raw_img_dir=os.path.join(RAW_DIR, 'train/images/'),
         proc_csv=os.path.join(PROC_DIR, 'train_labels.csv'),
         proc_img_dir=os.path.join(PROC_DIR, 'train_images/')
     )
     
     # Test Data
     process_dataset(
-        raw_csv=os.path.join(RAW_DIR, 'test_labels.csv'),
-        raw_img_dir=os.path.join(RAW_DIR, 'test_images/'),
+        raw_csv=os.path.join(RAW_DIR, 'test/annotations.csv'),
+        raw_img_dir=os.path.join(RAW_DIR, 'test/images/'),
         proc_csv=os.path.join(PROC_DIR, 'test_labels.csv'),
         proc_img_dir=os.path.join(PROC_DIR, 'test_images/')
     )
